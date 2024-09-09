bb1bb7ec522e8247c61605eafc6fb41f898a8144
Keven Soares <97615788+KevenSoares22@users.noreply.github.com>
2024-09-09 20:36:16 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -34,7 +34,6 @@ bool bfs(const vector<vector<int>>& capacity, vector<vector<int>>& flow, vector<
     return false;
 }
 
-
 int edmondsKarp(const vector<vector<int>>& capacity, int source, int sink) {
     int n = capacity.size();
     vector<vector<int>> flow(n, vector<int>(n, 0));
@@ -63,21 +62,22 @@ int edmondsKarp(const vector<vector<int>>& capacity, int source, int sink) {
 int main() {
     // Definindo a rede de fluxo para o problema
     vector<vector<int>> capacity = {
-        {0, 10, 5, 0, 0, 0, 0, 0, 0}, 
-        {0, 0, 15, 10, 0, 0, 0, 0, 0},  
-        {0, 0, 0, 10, 10, 0, 0, 0, 0},  
-        {0, 0, 0, 0, 15, 10, 0, 0, 0}, 
-        {0, 0, 0, 0, 0, 10, 10, 0, 0}, 
-        {0, 0, 0, 0, 0, 0, 10, 5, 0},  
-        {0, 0, 0, 0, 0, 0, 0, 10, 10}, 
-        {0, 0, 0, 0, 0, 0, 0, 0, 15},  
-        {0, 0, 0, 0, 0, 0, 0, 0, 0} 
+        // A  B  C  D  E  F  G  H  I
+        {0, 34, 0, 0, 35, 0, 0, 0, 0},  // A
+        {34, 0, 0, 20, 8, 0, 0, 0, 0},  // B
+        {0, 0, 0, 0, 8, 15, 0, 0, 0},  // C
+        {0, 20, 0, 0, 35, 10, 22, 18, 0},  // D
+        {35, 8, 8, 35, 0, 15, 0, 0, 0},  // E
+        {0, 0, 15, 10, 15, 0, 0, 35, 0},  // F
+        {0, 0, 0, 22, 0, 0, 0, 20, 48},  // G
+        {0, 0, 0, 18, 0, 35, 20, 0, 30},  // H
+        {0, 0, 0, 0, 0, 0, 48, 30, 0}  // I
     };
 
     int source = 0;  // A é o nó 0
     int sink = 8;    // I é o nó 8
 
-    cout << "Fluxo máximo: " << edmondsKarp(capacity, source, sink);
+    cout << "Fluxo máximo: " << edmondsKarp(capacity, source, sink) ;
 
     return 0;
 }
